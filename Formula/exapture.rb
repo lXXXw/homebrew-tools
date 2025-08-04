2ab97a526793787a8b455ede00416fba7ea997d3
lxxxw <lxxxw.vido@gmail.com>
2025-08-04 16:55:52 -0700

    wtf

--- Formula/exapture.rb
+++ Formula/exapture.rb
@@ -1,8 +1,8 @@
 class Exapture < Formula
   desc "The most accurate screenshot tool for macOS - Capture exact pixel dimensions with a real-time cursor-following overlay."
-  homepage "https://github.com/yourname/"
-  url "https://github.com/yourname/yourtool/releases/download/v1.0.0/yourtool-darwin-amd64.tar.gz"
-  sha256 "your_sha256_checksum"
+  homepage "https://github.com/lXXXw/exapture"
+  url "https://github.com/lXXXw/exapture/releases/latest/download/exapture"
+  sha256 "0eb6ff2500f118f403d7f293741ad7c6bd4b57920eeb4fb396d3a5c6a429490b"
   version "1.0.0"
   def install
     bin.install "exapture"
